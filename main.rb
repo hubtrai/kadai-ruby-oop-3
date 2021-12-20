d0b61f3c7768817646f2aa367b830f8dc7e9d03c
oda.nobunaga <smtsmbam3s13@gmail.com>
2021-12-20 10:33:19 +0000

    ruby-kadai3

---
+++ main.rb
@@ -0,0 +1,14 @@
+require './human'
+require './thinkable'
+
+tanaka_name = Human.new('田中 太郎',25,'電車')
+tanaka_name.say
+tanaka_name.think
+
+suzuki_name = Human.new('鈴木 次郎',30,'野球')
+suzuki_name.say
+suzuki_name.think
+
+sato_name  = Human.new('佐藤 花子',20,'映画')
+sato_name.say
+sato_name.think
